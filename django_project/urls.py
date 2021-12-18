e9348a34ac0498a81623be3c9366c6668c1f679b
Mirfayz <yusupovmirfayz465@gmail.com>
2021-12-18 18:13:12 +0300

    qo'shimcha o'zgarishlar

--- django_project/urls.py
+++ django_project/urls.py
@@ -26,10 +26,11 @@ urlpatterns = [
     path('',include('blog.urls')),
     path('register/',users_views.register,name='register'),
     path('profile/',users_views.profile,name='profile'),
-    path('login/',auth_views.LoginView.as_view,(template_name='users/login.html'),name='login'),
-    path('logout/',auth_views.LogoutView.as_view,(template_name='users/logout.html'),name='logout'), 
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'), 
 
 ]
 
 if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+        
--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 from django.contrib import messages
 from .forms import UserRegisterForm
-from django.contrib.auth.decorators import login_reuired
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -12,8 +12,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username=form. cleaned_data.get['username']
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Yangi Account yaratildi. Siz login qila olasiz')
+            return redirect('login')
     else:
         form=UserRegisterForm()
     return render(request,'users/register.html',{'form':form})
